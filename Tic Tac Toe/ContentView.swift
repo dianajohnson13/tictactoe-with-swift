9b4be284c959cfaaffac1938faa066e0365b1e06
Diana Johnson <dianajohnson13@gmail.com>
2023-05-12 13:46:08 -0700

    Code from a while ago

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Diana Johnson on 2/8/23.
+//
+
+import SwiftUI
+
+var playeOneScore = 0
+var playerTwoScore = 0
+
+let symbols = ["x", "o"]
+
+let winningCombos:Set = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            HStack{
+                Spacer()
+                VStack(alignment: .center){
+                    Text("Player One")
+                        .font(.title)
+                    Text("0").font(.title2).padding() // placeholder for score
+                }
+                Spacer()
+                VStack(alignment: .center){
+                    Text("Player Two")
+                        .font(.title)
+                    Text("3").font(.title2).padding()
+                    // placeholder for score
+                }
+                Spacer()
+            }
+            Spacer()
+            VStack {
+                HStack {
+                    Image("o").padding()
+                    Image("o").padding()
+                    Image("o").padding()
+                }
+                HStack {
+                    Image("o").padding()
+                    Image("o").padding()
+                    Image("o").padding()
+                }
+                HStack {
+                    Image("o").padding()
+                    Image("o").padding()
+                    Image("o").padding()
+                }
+            }
+    
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
